685876af5c594c6273600214959d2f0a2b748baf
varuntejreddy03 <152944384+varuntejreddy03@users.noreply.github.com>
2025-08-02 22:13:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -73,6 +73,8 @@ app.get("/attendance", async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
